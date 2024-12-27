0c2f047d05fa4a9cc08a503e0d3e006e07f07120
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2024-12-27 18:44:03 +0800

    Update disk_check_test.py

--- tests/disk_check_test.py
+++ tests/disk_check_test.py
@@ -180,6 +180,7 @@ class TestDiskCheck(object):
 
     @patch("disk_check.syslog.syslog")
     @patch("disk_check.subprocess.run")
+    @patch('os.access', return_value=True)
     @patch('os.statvfs', return_value=os.statvfs_result((4096, 4096, 1909350, 1491513, 0,
                                                          971520, 883302, 883302, 4096, 255)))
     def test_diskfull(self, mock_proc, mock_log, os_statvfs):
