118f051ebfe1f9d0de4ef401f8127426d23f56d8
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-01-14 16:23:47 +0800

    Add disk full check and overlay prefix

--- scripts/disk_check.py
+++ scripts/disk_check.py
@@ -30,6 +30,7 @@ Tidbit:
 
 import argparse
 import os
+import shutil
 import sys
 import syslog
 import subprocess
@@ -46,6 +47,9 @@ FREE_SPACE_THRESHOLD = 1024
 EVENTS_PUBLISHER_SOURCE = "sonic-events-host"
 EVENTS_PUBLISHER_TAG = "event-disk"
 events_handle = None
+
+DISK_RO_EVENT = "read_only"
+DISK_FULL_EVENT = "disk_full"
 
 chk_log_level = syslog.LOG_ERR
 
@@ -73,22 +77,28 @@ def event_pub(event):
     event_publish(events_handle, EVENTS_PUBLISHER_TAG, param_dict)
 
 
+def test_disk_full(dirs): 
+    for d in dirs:
+        space = os.statvfs(d)
+        if space.f_bavail < FREE_SPACE_THRESHOLD:
+            log_err("{} has no free disk space".format(d))
+            event_pub(DISK_FULL_EVENT)
+            return True
+        else:
+            log_debug("{} has enough disk space".format(d))
+
+    return False
+
+
 def test_writable(dirs): 
     for d in dirs:
         rw = os.access(d, os.W_OK)
         if not rw:
             log_err("{} is not read-write".format(d))
-            event_pub("read_only")
+            event_pub(DISK_RO_EVENT)
             return False
         else:
             log_debug("{} is Read-Write".format(d))
-            space = os.statvfs(d)
-            if space.f_bavail < FREE_SPACE_THRESHOLD:
-                log_err("{} has no free disk space".format(d))
-                event_pub("disk_full")
-                return False
-            else:
-                log_debug("{} has enough disk space".format(d))
 
     return True
 
@@ -112,7 +122,7 @@ def get_dname(path_name):
     return os.path.basename(os.path.normpath(path_name))
 
 
-def do_mnt(dirs):
+def do_mnt(dirs, overlay_prefix):
     if os.path.exists(UPPER_DIR):
         log_err("Already mounted")
         return 1
@@ -121,7 +131,7 @@ def do_mnt(dirs):
         try:
             os.mkdir(i)
         except OSError as error:
-            log_err("Failed to create {}".format(i))
+            log_err("Failed to create {}, error: {}".format(i, error))
             return 1
 
     for d in dirs:
@@ -131,7 +141,7 @@ def do_mnt(dirs):
         os.mkdir(d_upper)
         os.mkdir(d_work)
 
-        ret = run_cmd(["mount", "-t", "overlay", "overlay_{}".format(d_name),\
+        ret = run_cmd(["mount", "-t", "overlay", "{}_{}".format(overlay_prefix, d_name),\
                 "-o", "lowerdir={},upperdir={},workdir={}".format(d, d_upper, d_work), d])
         if ret:
             break
@@ -142,14 +152,35 @@ def do_mnt(dirs):
         log_info("{} are mounted as Read-Write".format(dirs))
     return ret
 
+def do_unmnt(dirs, overlay_prefix):
+    for d in dirs:
+        d_name = get_dname(d)
+        ret = run_cmd(["umount", "-l", "{}_{}".format(overlay_prefix, d_name)])
+        if ret:
+            break
 
-def is_mounted(dirs):
+    if ret:
+        log_err("Failed to umount {}".format(dirs))
+    else:
+        log_info("{} are unmounted".format(dirs))
+
+    for i in (UPPER_DIR, WORK_DIR):
+        try:
+            shutil.rmtree(i)
+        except OSError as error:
+            log_err("Failed to delete {}, error: {}".format(i, error))
+            return 1
+
+    return ret
+
+
+def is_mounted(dirs, overlay_prefix):
     if not os.path.exists(UPPER_DIR):
         return False
 
     onames = set()
     for d in dirs:
-        onames.add("overlay_{}".format(get_dname(d)))
+        onames.add("{}_{}".format(overlay_prefix, get_dname(d)))
 
     with open(MOUNTS_FILE, "r") as s:
         for ln in s.readlines():
@@ -164,12 +195,31 @@ def do_check(skip_mount, dirs):
     ret = 0
     if not test_writable(dirs):
         if not skip_mount:
-            ret = do_mnt(dirs)
+            ret = do_mnt(dirs, "overlay")
 
     # Check if mounted
-    if (not ret) and is_mounted(dirs):
+    if (not ret) and is_mounted(dirs, "overlay"):
         log_err("READ-ONLY: Mounted {} to make Read-Write".format(dirs))
-        event_pub("read_only")
+        event_pub(DISK_RO_EVENT)
+
+    if ret:
+        # When disk mounted, disk no free space issue also been fixed.
+        return ret
+
+    # Handle disk no free space case
+    if test_disk_full(dirs):
+        if not skip_mount:
+            ret = do_mnt(dirs, "overlay_disk_full")
+
+    # Check if mounted
+    if (not ret) and is_mounted(dirs, "overlay_disk_full"):
+        log_err("DISK-FULL: Mounted {} to make Read-Write".format(dirs))
+        event_pub(DISK_FULL_EVENT)
+
+    # Unmount when disk space issue fixed
+    if is_mounted(dirs, "overlay_disk_full") and not test_disk_full(["/"]):
+        log_debug("umount for disk space issue fixed")
+        do_unmnt(dirs, "overlay_disk_full")
 
     return ret
 
