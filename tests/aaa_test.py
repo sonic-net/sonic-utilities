9b065003933c60912307e90f665b4ac47c6583fc
toby chen <49122725+651477787@users.noreply.github.com>
2025-02-14 09:15:17 +0800

    Update aaa_test.py
    add unit test case for  reaching max tacacs server

--- tests/aaa_test.py
+++ tests/aaa_test.py
@@ -284,6 +284,26 @@ class TestAaa(object):
         assert result.exit_code == 0
         assert result.output == show_aaa_disable_accounting_output
 
+    def test_config_aaa_tacacs_reach_maxsize(self, get_cmd_module):
+        (config, show) = get_cmd_module
+        runner = CliRunner()
+        db = Db()
+        db.cfgdb.delete_table("TACPLUS_SERVER")
+
+        # test tacacs reach max size
+        runner.invoke(config.config.commands["tacacs"].commands["add"], ["10.10.10.11"], obj=obj)
+        runner.invoke(config.config.commands["tacacs"].commands["add"], ["10.10.10.12"], obj=obj)
+        runner.invoke(config.config.commands["tacacs"].commands["add"], ["10.10.10.13"], obj=obj)
+        runner.invoke(config.config.commands["tacacs"].commands["add"], ["10.10.10.14"], obj=obj)
+        runner.invoke(config.config.commands["tacacs"].commands["add"], ["10.10.10.15"], obj=obj)
+        runner.invoke(config.config.commands["tacacs"].commands["add"], ["10.10.10.16"], obj=obj)
+        runner.invoke(config.config.commands["tacacs"].commands["add"], ["10.10.10.17"], obj=obj)
+        runner.invoke(config.config.commands["tacacs"].commands["add"], ["10.10.10.18"], obj=obj)
+        result = runner.invoke(config.config.commands["tacacs"].commands["add"], ["10.10.10.19"], obj=db)
+        print(result.exit_code)
+        assert result.exit_code != 0
+        assert "Error: tacacs server reached max size 8" in result.output
+        
     @patch("validated_config_db_connector.device_info.is_yang_config_validation_enabled", mock.Mock(return_value=True))
     @patch("config.validated_config_db_connector.ValidatedConfigDBConnector.validated_set_entry", mock.Mock(side_effect=JsonPatchConflict))
     def test_config_aaa_tacacs_delete_yang_validation(self):
