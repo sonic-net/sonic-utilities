96fb71b49c53110cd0df0e107578142a0bc95ae1
Xincun Li <xincun.li@microsoft.com>
2025-03-28 17:35:23 -0700

    fxi value

--- tests/config_override_test.py
+++ tests/config_override_test.py
@@ -443,7 +443,7 @@ class TestConfigOverrideMultiasic(object):
             if ns != config.DEFAULT_NAMESPACE and ns != HOST_NAMESPACE:
                 asic_id = config_db.get_config()['DEVICE_METADATA']['localhost'].get('asic_id')
                 # default asic_id is 01:00:00
-                assert asic_id == "01:00:00"
+                assert asic_id == "01:00:0"
 
     def test_masic_missig_localhost_override(self):
         def read_json_file_side_effect(filename):
