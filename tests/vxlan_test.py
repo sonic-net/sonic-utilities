a8cac758bdd653eda1b6d3c658da40b4be97f0d9
Soumya Mishra <soumyamishra@microsoft.com>
2025-04-08 21:07:03 -0700

    Reapply "fix sanity tests"
    This reverts commit a2a678ecef8d8eef4730af03d96862c773d7cee4.

--- tests/vxlan_test.py
+++ tests/vxlan_test.py
@@ -438,7 +438,7 @@ class TestVnet(object):
         assert result.exit_code == 0
         assert "Vnet_2000" in result.output
         assert "1234-56-7890-1234" in result.output
-#        assert "Ethernet4" in result.output
+        assert "Ethernet4" in result.output
         assert "Vlan100" in result.output
 
     def test_show_vnet_guid(self):
@@ -453,10 +453,12 @@ class TestVnet(object):
         assert "Vnet_2000" in result.output
         assert "1234-56-7890-1234" in result.output
         assert "tunnel1" in result.output
-#        assert "Ethernet4" in result.output
+        assert "Ethernet4" in result.output
         assert "Vlan100" in result.output
 
     @classmethod
     def teardown_class(cls):
         os.environ['UTILITIES_UNIT_TESTING'] = "0"
         print("TEARDOWN")
+        from .mock_tables import dbconnector
+        dbconnector.dedicated_dbs = {}
