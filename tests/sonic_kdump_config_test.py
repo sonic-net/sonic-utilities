16b4b9cbff44f3f99d10c1887c0605bd27ef8ee9
Muhammad Ali Hussnain <91005947+muhammadalihussnain@users.noreply.github.com>
2025-04-09 18:32:01 +0000

    code cover

--- tests/sonic_kdump_config_test.py
+++ tests/sonic_kdump_config_test.py
@@ -25,13 +25,26 @@ sonic_kdump_config = load_module_from_source("sonic_kdump_config", sonic_kdump_c
 
 def create_parser():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--num_dumps', nargs='?', type=int, action='store', default=3,
+                        help='Maximum number of kernel dump files stored')
     parser.add_argument('--remote', action='store_true', help='enable remote ssh')
+    parser.add_argument('--ssh_string', nargs='?', type=str, action='store', default=None,
+                        help='ssh_string for remote kdump')
     return parser
 
-
-class TestRemoteArg(unittest.TestCase):
+class TestSonicKdumpConfig(unittest.TestCase):
     def setUp(self):
         self.parser = create_parser()
+
+    def test_num_dumps_default(self):
+        """Test the default value of --num_dumps"""
+        args = self.parser.parse_args([])
+        self.assertEqual(args.num_dumps, 3)
+
+    def test_num_dumps_provided(self):
+        """Test --num_dumps with a provided value"""
+        args = self.parser.parse_args(['--num_dumps', '5'])
+        self.assertEqual(args.num_dumps, 5)
 
     def test_remote_default(self):
         """Test the default value of --remote"""
@@ -42,6 +55,16 @@ class TestRemoteArg(unittest.TestCase):
         """Test --remote with the flag provided"""
         args = self.parser.parse_args(['--remote'])
         self.assertTrue(args.remote)
+
+    def test_ssh_string_default(self):
+        """Test the default value of --ssh_string"""
+        args = self.parser.parse_args([])
+        self.assertIsNone(args.ssh_string)
+
+    def test_ssh_string_provided(self):
+        """Test --ssh_string with a provided value"""
+        args = self.parser.parse_args(['--ssh_string', 'user@host'])
+        self.assertEqual(args.ssh_string, 'user@host')
 
 class TestSonicKdumpConfig(unittest.TestCase):
     @classmethod
