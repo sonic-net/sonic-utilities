aea14308592e9f5f94a3ea481dc50c9266a8ca8f
Jakub Marciniszyn <jmarcini@cisco.com>
2025-05-28 14:39:41 +0100

    Fix review comments

--- tests/fast_reboot_test.py
+++ tests/fast_reboot_test.py
@@ -1,25 +1,28 @@
 import os
-import tempfile
+import pytest
 import shutil
 import subprocess
+import tempfile
+
 
 class TestFastReboot:
-    def test_fast_reboot_without_login_shell(self):
+    @pytest.mark.parametrize(
+        "working_script,failing_script", [
+            pytest.param('whoami', 'logname', id='without_logname'),
+            pytest.param('logname', 'whoami', id='without_whoami'),
+        ]
+    )
+    def test_fast_reboot_user(self, working_script, failing_script):
         test_path = os.path.dirname(os.path.abspath(__file__))
         fast_reboot = os.path.join(test_path, '..', 'scripts', 'fast-reboot')
-
         with tempfile.TemporaryDirectory() as tmp_dir:
-            whoami = os.path.join(tmp_dir, 'whoami')
-            with open(whoami, 'w') as f:
+            working_script_path = os.path.join(tmp_dir, working_script)
+            with open(working_script_path, 'w') as f:
                 f.write('echo root')
-                os.chmod(whoami, 0o700)
-
-            shutil.copy('/bin/false', os.path.join(tmp_dir, 'logname'))
+                os.chmod(working_script_path, 0o700)
+            shutil.copy('/bin/false', os.path.join(tmp_dir, failing_script))
             shutil.copy('/bin/true', os.path.join(tmp_dir, 'sonic-cfggen'))
-
             env = os.environ.copy()
             env['PATH'] = f"{tmp_dir}:{env.get('PATH', '')}"
-
             res = subprocess.run([fast_reboot, '-h'], env=env)
-
         assert res.returncode == 0
