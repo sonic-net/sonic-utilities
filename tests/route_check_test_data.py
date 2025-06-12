0bce0909c42bd6d9cf3b6cc0da46920b12317d7c
BYGX-wcr <wcr@live.cn>
2025-06-12 21:32:26 +0000

    fix formatting

--- tests/route_check_test_data.py
+++ tests/route_check_test_data.py
@@ -223,11 +223,11 @@ TEST_DATA = {
                         "10.10.196.12/31": {"ifname": "portchannel0"},
                         "10.10.196.20/31": {"ifname": "portchannel0"},
                         "10.10.196.30/31": {"ifname": "lo"},
-                        "10.10.197.1" : {"ifname": "Ethernet-IB0", "nexthop": "0.0.0.0"},
-                        "2603:10b0:503:df5::1" : {"ifname": "Ethernet-IB0", "nexthop": "::"},
-                        "100.0.0.2/32" : {"ifname": "Ethernet-IB0", "nexthop": "0.0.0.0" },
-                        "2064:100::2/128" : {"ifname": "Ethernet-IB0", "nexthop": "::" },
-                        "101.0.0.0/24" : {"ifname": "Ethernet-IB0", "nexthop": "100.0.0.2"}
+                        "10.10.197.1": {"ifname": "Ethernet-IB0", "nexthop": "0.0.0.0"},
+                        "2603:10b0:503:df5::1": {"ifname": "Ethernet-IB0", "nexthop": "::"},
+                        "100.0.0.2/32": {"ifname": "Ethernet-IB0", "nexthop": "0.0.0.0"},
+                        "2064:100::2/128": {"ifname": "Ethernet-IB0", "nexthop": "::"},
+                        "101.0.0.0/24": {"ifname": "Ethernet-IB0", "nexthop": "100.0.0.2"}
                     },
                     INTF_TABLE: {
                         "PortChannel1013:10.10.196.24/31": {},
@@ -307,9 +307,9 @@ TEST_DATA = {
                         "10.10.196.30/31": {"ifname": "lo"}
                     },
                     VNET_ROUTE_TABLE: {
-                        "Vnet1:30.1.10.0/24": {"ifname": "Vlan3001" },
-                        "Vnet1:50.1.1.0/24": {"ifname": "Vlan3001" },
-                        "Vnet1:50.2.2.0/24": {"ifname": "Vlan3001" }
+                        "Vnet1:30.1.10.0/24": {"ifname": "Vlan3001"},
+                        "Vnet1:50.1.1.0/24": {"ifname": "Vlan3001"},
+                        "Vnet1:50.2.2.0/24": {"ifname": "Vlan3001"}
                     },
                     INTF_TABLE: {
                         "PortChannel1013:10.10.196.24/31": {},
@@ -669,8 +669,8 @@ TEST_DATA = {
                     ROUTE_TABLE: {
                         "10.10.196.12/31": {"ifname": "portchannel0"},
                         "10.10.196.20/31": {"ifname": "portchannel0"},
-                        "192.168.0.101/32": {"ifname": "tun0" },
-                        "192.168.0.103/32": {"ifname": "tun0" },
+                        "192.168.0.101/32": {"ifname": "tun0"},
+                        "192.168.0.103/32": {"ifname": "tun0"},
                     },
                     INTF_TABLE: {
                         "PortChannel1013:90.10.196.24/31": {},
