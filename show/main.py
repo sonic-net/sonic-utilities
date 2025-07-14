7759e4ecb099d94ffc52f4a0a59dcd183d0c0404
Connor Roos <croos@s-build-sonic-01.mts.labs.mlnx>
2025-07-14 21:06:21 +0000

    Add CLI changes

--- show/main.py
+++ show/main.py
@@ -576,6 +576,41 @@ def snmpagentaddress (ctx):
         body.append([agent[0], agent[1], agent[2]])
     click.echo(tabulate(body, header))
 
+@interfaces.command()
+@click.argument('interfacename', required=False)
+def tx_error(interfacename):
+    """Show Interface tx_error information"""
+
+    state_db = SonicV2Connector(host='127.0.0.1')
+    state_db.connect(state_db.STATE_DB, False)   # Make one attempt only
+    TABLE_NAME_SEPARATOR = '|'
+    prefix_statedb = "TX_ERR_STATE|"
+    _hash = '{}{}'.format(prefix_statedb, '*')
+    # DBInterface keys() method
+    txerr_keys = state_db.keys(state_db.STATE_DB, _hash)
+    appl_db = SonicV2Connector(host='127.0.0.1')
+    appl_db.connect(appl_db.APPL_DB, False)
+    prefix_appldb = "TX_ERR_APPL:"
+    _hash = '{}{}'.format(prefix_statedb, "*")
+    txerr_appl_keys = appl_db.keys(appl_db.APPL_DB, _hash)
+    table = []
+    for k in txerr_keys:
+        k = k.replace(prefix_statedb, "")
+        r = []
+        r.append(k)
+
+        r.append(state_db.get(state_db.STATE_DB, prefix_statedb + k, "tx_status"))
+        entry = appl_db.get_all(appl_db.APPL_DB, prefix_appldb + k)
+        if 'tx_error_stati' not in entry:
+            r.append("")
+        else:
+            r.append(entry['tx_error_stati'])
+
+        table.append(r)
+
+    header = ['Port', 'status', 'statistics']
+    click.echo(tabulate(table, header))
+
 #
 # 'snmptrap' group ("show snmptrap ...")
 #
