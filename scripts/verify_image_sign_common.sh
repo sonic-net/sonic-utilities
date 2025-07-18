f65b7b26541c9a388f14ddd6c666a118f05d8730
Brad House <brad@brad-house.com>
2025-07-18 14:35:25 -0400

    Secureboot: Image signing verification
    The DB Key is not guaranteed to be a CARoot.  It is possible to have
    the DB Key be an intermediate as it is trusted via the EFI signing
    methods which do not use the normal PKI methodology.
    
    This adds a secondary signing verification, the original plus the
    above described method.
    
    Signed-off-by: Brad House <bhouse@nexthop.ai>

--- scripts/verify_image_sign_common.sh
+++ scripts/verify_image_sign_common.sh
@@ -15,7 +15,8 @@ verify_image_sign_common() {
         no_check_time="-noattr"
     fi
     
-    # making sure image verification is supported
+    # This command verifies the signing as a complete certificate chain root of trust and requires the DB Key to
+    # be a self-signed root, but the image signed with an intermediate embedded into the certificate.
     EFI_CERTS_DIR=/tmp/efi_certs
     RESULT="CMS Verification Failure"
     LOG=$(openssl cms -verify $no_check_time -noout -CAfile $EFI_CERTS_DIR/cert.pem -binary -in ${CMS_SIG_FILE} -content ${DATA_FILE} -inform pem 2>&1 > /dev/null )
@@ -28,7 +29,22 @@ verify_image_sign_common() {
         # Exit without error if any success signature verification.
         return 0
     fi
-    
+
+    # This is a backup signature verification method which does NOT trust the certificate embedded into the pkcs7
+    # signature (via the -nointern flag) and assumes the DB key directly signed the image (via the -certfile flag).
+    # Since the DB key is trusted, it doesn't need to be a root CA so we turn off root CA verification with the
+    # -noverify flag.
+    LOG=$(openssl cms -verify $no_check_time -noout -certfile $EFI_CERTS_DIR/cert.pem -binary -nointern -noverify -in ${CMS_SIG_FILE} -content ${DATA_FILE} -inform pem 2>&1 > /dev/null )
+    VALIDATION_RES=$?
+    if [ $VALIDATION_RES -eq 0 ]; then
+        RESULT="CMS Verified OK"
+        if  [ -d "${TMP_DIR}" ]; then rm -rf ${TMP_DIR}; fi
+        echo "verification ok:$RESULT"
+        # No need to continue.
+        # Exit without error if any success signature verification.
+        return 0
+    fi
+
     if  [ -d "${TMP_DIR}" ]; then rm -rf ${TMP_DIR}; fi
     return 1
 }
