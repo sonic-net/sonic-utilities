e2b9eab937a7e02ec682fcbcdefe23c1fa7ed284
BYGX-wcr <wcr@live.cn>
2025-10-31 20:04:12 +0000

    fix for pre-commit check

--- show/srv6.py
+++ show/srv6.py
@@ -14,6 +14,7 @@ def srv6():
     """Show SRv6 related information"""
     pass
 
+
 # `show srv6 locators`
 @srv6.command()
 @click.argument("locator", required=False)
@@ -21,7 +22,7 @@ def locators():
     config_db = ConfigDBConnector()
     config_db.connect()
     data = config_db.get_table(CONFIG_DB_MY_LOCATORS_TABLE)
-    
+
     header = ["Locator", "Prefix", "Block Len", "Node Len", "Func Len"]
     table = []
     for k in natsorted(data.keys()):
@@ -34,7 +35,7 @@ def locators():
             entry.get("func_len", 16)
         ])
     click.echo(tabulate(table, header))
-        
+
 
 # `show srv6 static-sids`
 @srv6.command()
@@ -43,22 +44,22 @@ def static_sids(sid):
     config_db = ConfigDBConnector()
     config_db.connect()
     data = config_db.get_table(CONFIG_DB_MY_SID_TABLE)
-    
+
     # parse the keys to get the locator for each sid
     sid_dict = dict()
     for k, v in data:
-        if sid and  sid not in k:
+        if sid and sid not in k:
             # skip not relevant SIDs
             continue
         if "|" not in k:
             # skip SIDs that does not have locators
             continue
-        
+
         loc = k.split("|")[0]
         sid_prefix = k.split("|")[1]
         v["locator"] = loc
         sid_dict[sid_prefix] = v
-        
+
     # query ASIC_DB to check whether the SID is offloaded to the ASIC
     db = SonicV2Connector(host="localhost")
     db.connect(db.ASIC_DB)
@@ -76,8 +77,7 @@ def static_sids(sid):
         func_len = int(fields["function_len"])
         sid_prefix = sid_ip + f"/{block_len + node_len + func_len}"
         asic_sids.add(sid_prefix)
-        
-        
+
     # format and print the sid dictionaries
     header = ["SID", "Locator", "Action", "Decap DSCP Mode", "Decap VRF", "Offloaded"]
     table = []
