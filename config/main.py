207af46f091eb61c33190fa3a8d6f81f1fc9ba4b
Stephen Sun <stephens@nvidia.com>
2025-11-04 07:16:08 +0000

    Fix community review comments
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- config/main.py
+++ config/main.py
@@ -1121,31 +1121,28 @@ def interface_has_mirror_config(ctx, mirror_table, dst_port, src_port, direction
 
 def is_port_mirror_capability_supported(direction, namespace=None):
     """ Check if port mirror capability is supported for the given direction """
-    try:
-        state_db = SonicV2Connector(host='127.0.0.1')
-        state_db.connect(state_db.STATE_DB, False)
-        entry_name = "SWITCH_CAPABILITY|switch"
+    state_db = SonicV2Connector(use_unix_socket_path=True, namespace=namespace)
+    state_db.connect(state_db.STATE_DB, False)
+    entry_name = "SWITCH_CAPABILITY|switch"
 
-        # If no direction is specified, check both ingress and egress capabilities
-        if not direction:
-            ingress_supported = state_db.get(state_db.STATE_DB, entry_name, "PORT_INGRESS_MIRROR_CAPABLE")
-            egress_supported = state_db.get(state_db.STATE_DB, entry_name, "PORT_EGRESS_MIRROR_CAPABLE")
-            return ingress_supported == "true" and egress_supported == "true"
+    # If no direction is specified, check both ingress and egress capabilities
+    if not direction:
+        ingress_supported = state_db.get(state_db.STATE_DB, entry_name, "PORT_INGRESS_MIRROR_CAPABLE")
+        egress_supported = state_db.get(state_db.STATE_DB, entry_name, "PORT_EGRESS_MIRROR_CAPABLE")
+        return ingress_supported == "true" and egress_supported == "true"
 
-        if direction in ['rx', 'both']:
-            ingress_supported = state_db.get(state_db.STATE_DB, entry_name, "PORT_INGRESS_MIRROR_CAPABLE")
-            if ingress_supported != "true":
-                return False
+    if direction in ['rx', 'both']:
+        ingress_supported = state_db.get(state_db.STATE_DB, entry_name, "PORT_INGRESS_MIRROR_CAPABLE")
+        if ingress_supported != "true":
+            return False
 
-        if direction in ['tx', 'both']:
-            egress_supported = state_db.get(state_db.STATE_DB, entry_name, "PORT_EGRESS_MIRROR_CAPABLE")
-            if egress_supported != "true":
-                return False
+    if direction in ['tx', 'both']:
+        egress_supported = state_db.get(state_db.STATE_DB, entry_name, "PORT_EGRESS_MIRROR_CAPABLE")
+        if egress_supported != "true":
+            return False
 
-        return True
-    except Exception:
-        # If we can't check capability, assume it's supported for backward compatibility
-        return True
+    return True
+
 
 def validate_mirror_session_config(config_db, session_name, dst_port, src_port, direction):
     """ Check if SPAN mirror-session config is valid """
@@ -1167,7 +1164,6 @@ def validate_mirror_session_config(config_db, session_name, dst_port, src_port, 
 
         if interface_is_in_vlan(vlan_member_table, dst_port):
             ctx.fail("Error: Destination Interface {} has vlan config".format(dst_port))
-
 
         if interface_is_in_portchannel(portchannel_member_table, dst_port):
             ctx.fail("Error: Destination Interface {} has portchannel config".format(dst_port))
@@ -1192,7 +1188,7 @@ def validate_mirror_session_config(config_db, session_name, dst_port, src_port, 
     # Check port mirror capability before allowing configuration
     # If direction is provided, check the specific direction
     if not is_port_mirror_capability_supported(direction):
-        ctx.fail("Error: Port mirror direction '{}' is not supported by the ASIC".format(direction))
+        ctx.fail("Error: Port mirror direction '{}' is not supported by the ASIC".format(direction if direction else 'both'))
 
     return True
 
