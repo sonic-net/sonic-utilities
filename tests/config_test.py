4e3c88c474766cae8c362e85f00ad21892f869d0
Hemanth Kumar Tirupati <htirupati@nvidia.com>
2026-02-20 19:56:32 +0000

    Wait for monit monitor <service> operation to complete during config reload
    Signed-off-by: Hemanth Kumar Tirupati <htirupati@nvidia.com>

--- tests/config_test.py
+++ tests/config_test.py
@@ -318,6 +318,8 @@ def mock_run_command_side_effect(*args, **kwargs):
             return f'{datetime.datetime.now()}', 0
         elif command == 'sudo systemctl show --no-pager networking -p ExecMainExitTimestamp --value':
             return f'{datetime.datetime.now()}', 0
+        elif command.startswith('sudo monit status'):
+            return '  monitoring status            Monitored\n', 0
         else:
             return '', 0
 
@@ -1067,7 +1069,7 @@ class TestLoadMinigraph(object):
             # Verify "systemctl reset-failed" is called for services under sonic.target
             mock_run_command.assert_any_call(['systemctl', 'reset-failed', 'swss'])
             mock_run_command.assert_any_call(['systemctl', 'reset-failed', 'pmon'])
-            assert mock_run_command.call_count == 19
+            assert mock_run_command.call_count == 21
 
     @mock.patch('sonic_py_common.device_info.get_paths_to_platform_and_hwsku_dirs',
                 mock.MagicMock(return_value=("dummy_path", None)))
