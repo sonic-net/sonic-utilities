26edbb4da77e6304e443251c9fd9dc8b488af736
eder <eder@arista.com>
2026-02-26 12:42:11 -0800

    Show error message when FEC histogram is not available
    When the SAI_PORT_STAT_IF_IN_FEC_CODEWORD_ERRORS_S[0-15] counter is not
    available on COUNTERS_DB, the command `show interfaces fec-histogram` prints
    all bins with value equals zero without any error message. This change fixes
    this behavior.
    
    Signed-off-by: eder <eder@arista.com>

--- show/interfaces/__init__.py
+++ show/interfaces/__init__.py
@@ -864,7 +864,7 @@ def fetch_fec_histogram(db, namespace, port_oid_map, target_port):
     if asic_db_kvp is not None:
 
         fec_errors = {f'BIN{i}': asic_db_kvp.get
-                      (f'SAI_PORT_STAT_IF_IN_FEC_CODEWORD_ERRORS_S{i}', '0') for i in range(16)}
+                      (f'SAI_PORT_STAT_IF_IN_FEC_CODEWORD_ERRORS_S{i}', 'UNDEFINED') for i in range(16)}
 
         # Prepare the data for tabulation
         table_data = [(bin_label, error_value) for bin_label, error_value in fec_errors.items()]
@@ -874,6 +874,11 @@ def fetch_fec_histogram(db, namespace, port_oid_map, target_port):
 
         # Print FEC histogram using tabulate
         click.echo(tabulate(table_data, headers=headers))
+
+        # Show a message where there are missing histogram data
+        bins_count = sum(key.startswith('SAI_PORT_STAT_IF_IN_FEC_CODEWORD_ERRORS_S') for key in asic_db_kvp)
+        if bins_count == 0:
+            click.echo('FEC histogram data for port {} not found'.format(target_port), err=True)
     else:
         click.echo('No kvp found in ASIC DB for port {}, exiting'.format(target_port), err=True)
         raise click.Abort()
