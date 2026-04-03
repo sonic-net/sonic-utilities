48c6f1b97706a92dbcc846f6257fffa03c1349f5
liangqianqian <qianqian@micasnetworks.com>
2026-04-03 09:34:10 +0000

    [clear|stp] Fix stp clear command using mstpctl in stp container
    Signed-off-by: liangqianqian <qianqian@micasnetworks.com>

--- clear/stp.py
+++ clear/stp.py
@@ -17,7 +17,7 @@ def spanning_tree(ctx):
 @click.pass_context
 def stp_clr_stats(ctx):
     if ctx.invoked_subcommand is None:
-        command = 'sudo stpctl clrstsall'
+        command = ["sudo","docker","exec","stp","stpctl","clrstsall"]
         clicommon.run_command(command)
 
 
@@ -25,7 +25,9 @@ def stp_clr_stats(ctx):
 @click.argument('interface_name', metavar='<interface_name>', required=True)
 @click.pass_context
 def stp_clr_stats_intf(ctx, interface_name):
-    command = 'sudo stpctl clrstsintf ' + interface_name
+    command = ["sudo","docker","exec","stp","stpctl","clrstsintf"]
+    if interface_name:
+        command += [str(interface_name)]
     clicommon.run_command(command)
 
 
@@ -33,7 +35,9 @@ def stp_clr_stats_intf(ctx, interface_name):
 @click.argument('vlan_id', metavar='<vlan_id>', required=True)
 @click.pass_context
 def stp_clr_stats_vlan(ctx, vlan_id):
-    command = 'sudo stpctl clrstsvlan ' + vlan_id
+    command = ["sudo","docker","exec","stp","stpctl","clrstsvlan"]
+    if vlan_id:
+        command += [str(vlan_id)]
     clicommon.run_command(command)
 
 
@@ -42,5 +46,7 @@ def stp_clr_stats_vlan(ctx, vlan_id):
 @click.argument('interface_name', metavar='<interface_name>', required=True)
 @click.pass_context
 def stp_clr_stats_vlan_intf(ctx, vlan_id, interface_name):
-    command = 'sudo stpctl clrstsvlanintf ' + vlan_id + ' ' + interface_name
+    command = ["sudo","docker","exec","stp","stpctl","clrstsvlanintf"]
+    if vlan_id and interface_name:
+        command += [str(vlan_id), str(interface_name)]
     clicommon.run_command(command)
