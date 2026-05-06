3c061537992113f40236234b81f644336b9d9a24
Qiana <Qianqian@micasnetworks.com>
2026-05-06 16:47:04 +0800

    fix: Address static check errors from review
    Signed-off-by: liangqianqian <qianqian@micasnetworks.com>

--- clear/stp.py
+++ clear/stp.py
@@ -25,9 +25,7 @@ def stp_clr_stats(ctx):
 @click.argument('interface_name', metavar='<interface_name>', required=True)
 @click.pass_context
 def stp_clr_stats_intf(ctx, interface_name):
-    command = ["sudo","docker","exec","stp","stpctl","clrstsintf"]
-    if interface_name:
-        command += [str(interface_name)]
+    command = ["sudo","docker","exec","stp","stpctl","clrstsintf", str(interface_name)]
     clicommon.run_command(command)
 
 
@@ -35,9 +33,7 @@ def stp_clr_stats_intf(ctx, interface_name):
 @click.argument('vlan_id', metavar='<vlan_id>', required=True)
 @click.pass_context
 def stp_clr_stats_vlan(ctx, vlan_id):
-    command = ["sudo","docker","exec","stp","stpctl","clrstsvlan"]
-    if vlan_id:
-        command += [str(vlan_id)]
+    command = ["sudo","docker","exec","stp","stpctl","clrstsvlan", str(vlan_id)]
     clicommon.run_command(command)
 
 
@@ -46,7 +42,5 @@ def stp_clr_stats_vlan(ctx, vlan_id):
 @click.argument('interface_name', metavar='<interface_name>', required=True)
 @click.pass_context
 def stp_clr_stats_vlan_intf(ctx, vlan_id, interface_name):
-    command = ["sudo","docker","exec","stp","stpctl","clrstsvlanintf"]
-    if vlan_id and interface_name:
-        command += [str(vlan_id), str(interface_name)]
+    command = ["sudo","docker","exec","stp","stpctl","clrstsvlanintf", str(vlan_id), str(interface_name)]
     clicommon.run_command(command)
