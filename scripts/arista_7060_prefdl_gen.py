4b5c27c69d1fd1ac95932d4f1f8fe30bc140ee8e
Justin Wong <jvwong@arista.com>
2026-09-02 18:49:23 +0000

    Added docstring for Python script
    Signed-off-by: Justin Wong <jvwong@arista.com>

--- scripts/arista_7060_prefdl_gen.py
+++ scripts/arista_7060_prefdl_gen.py
@@ -2,6 +2,15 @@ from arista.components.eeprom import I2cEeprom
 from arista.core.platform import loadPrerequisites
 from arista.core.types import I2cBus
 
+"""Read system information from EEPROM via I2C
+
+This script reads system information from the EEPROM via I2C.
+
+This script is only intended to be ran on Arista-7060-32S
+  devices on or before the SONiC 202605 release.
+"""
+
+
 IDENT_BUS_NAME = 'SMBus PIIX4 adapter port 1 at 0b20'
 
 loadPrerequisites()
